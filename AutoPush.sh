c0a1310e3680b9a580ae1be3d1c85d2989e09ae8
joelwembo <joelotepawembo@gmail.com>
2022-01-09 01:34:23 +0800

    fd

---
+++ AutoPush.sh
@@ -0,0 +1,16 @@
+git status
+
+git add .
+
+# echo 'Enter the commit message:'
+#commitMessage = 'cpython and c compilation with python'
+
+git commit -m "cpython and c compilation with python"
+
+#echo 'Enter the name of the branch:'
+#read branch
+
+#git push origin $branch
+git push origin
+
+read
--- CPython-mix/myCprogram.c
+++ CPython-mix/myCprogram.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 
-int main(void) {
+int main(int argc, char *argv[])
+{
+    if (argc < 4)
+    {
+        printf ("Please enter the server IP address"
+                " and range of ports to be scanned\n");
+        printf ("USAGE: %s IPv4 First_Port Last_Port\n", 
+                argv[0]);
+        exit(1);
+    }
+    char tIP[16] = {0};
+    strcpy(tIP, argv[1]); // Copy the IPv4 address
+    char First_Port[6] = {0};
+    strcpy(First_Port, argv[2]); // Copy the start_port
+    char Last_Port[6] = {0};
+    strcpy(Last_Port, argv[3]); // Copy the end_port
 
-   
-    printf("hello,  we are running this c program \n");
-    printf("----------------- C Program----------------\n");
+    // Start port-scanner
+    port_scanner(tIP, First_Port, Last_Port);
     return 0;
 }
