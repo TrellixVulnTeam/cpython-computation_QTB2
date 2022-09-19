1be3f310f5954788903a099260e0de6e6f83b434
Joel Wembo <joelotepawembo@gmail.com>
2022-09-19 18:43:00 +0800

    codebase update

--- AutoPush.sh
+++ AutoPush.sh
@@ -4,13 +4,12 @@ sudo git add .
 
 # echo 'Enter the commit message:'
 #commitMessage = 'cpython and c compilation with python'
-
 sudo git commit -m "cpython and c compilation with python"
 
 #echo 'Enter the name of the branch:'
 #read branch
 
 #git push origin $branch
-sudo git push origin
+sudo git push origin main
 
 read
